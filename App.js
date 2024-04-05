b6983e12fde3b13863b4dcd7ca96eed6a0d2c46f
hsinyuwww <wen.hsi@northeastern.edu>
2024-04-04 22:08:19 -0400

    update App.js

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import express from "express";
 import cors from "cors";
-import ModuleRoutes from "./modules/routes.js";
-import CourseRoutes from "./courses/routes.js";
-import AssignmentRoutes from "./assignments/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import CourseRoutes from "./Courses/routes.js";
+import AssignmentRoutes from "./Assignments/routes.js";
 
 const app = express();
 app.use(
